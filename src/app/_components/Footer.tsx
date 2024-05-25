e9f9133ae110132ffa6e7a419b6fe0a0f685f2ce
Facundo Marcó del Pont <facundo.marcodelpont@replypro.io>
2024-05-25 17:55:57 -0300

    adoc

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -6,11 +6,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-bordeaux px-12 p-4 flex flex-col gap-4'>
-      <div className='flex items-center justify-center gap-10 uppercase'>
+    <footer className='bg-bordeaux px-12 p-4'>
+      <div className='flex items-center justify-center gap-6 sm:gap-10 uppercase'>
         <Separator className='w-[200px] h-[0.5px] bg-white' />
-        <Logo variant='white_mini' />
-        Construyendo futuro
+        <Logo variant='white_mini' className='min-w-[60px]' />
+        <p className='hidden sm:block'>
+          Construyendo
+          <br />
+          futuro
+        </p>
         <Separator className='w-[200px] h-[0.5px] bg-white' />
       </div>
     </footer>
